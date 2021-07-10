ea4af1ccdf8461f03971fc80b57718a2e19b20dd
richard <yaros0914@gmail.com>
2021-07-10 12:07:17 +0900

    update_2021-07-10 12:07:17

--- arduino_buzzuer_test/arduino_buzzuer_test.ino
+++ arduino_buzzuer_test/arduino_buzzuer_test.ino
@@ -13,19 +13,19 @@ void setup() {
 
 void loop() {
 
-       //tone(pin_B,262,100);//262 숫자는 4옥타브 도 음, 뒤에 100은 재생시간
-        digitalWrite(led_Mom, HIGH);
-        digitalWrite(led_Windy, LOW);
-        digitalWrite(led_Dad, LOW);
-        delay(time);
-        //tone(pin_B,294,100);//294 숫자는 4옥타브 레 음 
-        digitalWrite(led_Mom, LOW);
-        digitalWrite(led_Windy, HIGH);
-        digitalWrite(led_Dad, LOW);
-        delay(time);
-        //tone(pin_B,392,100);//392 숫자는 4옥타브 솔 음 
-        digitalWrite(led_Mom, LOW);
-        digitalWrite(led_Windy, LOW);
-        digitalWrite(led_Dad, HIGH);
-        delay(time);
+//       //tone(pin_B,262,100);//262 숫자는 4옥타브 도 음, 뒤에 100은 재생시간
+//        digitalWrite(led_Mom, HIGH);
+//        digitalWrite(led_Windy, LOW);
+//        digitalWrite(led_Dad, LOW);
+//        delay(time);
+//        //tone(pin_B,294,100);//294 숫자는 4옥타브 레 음 
+//        digitalWrite(led_Mom, LOW);
+//        digitalWrite(led_Windy, HIGH);
+//        digitalWrite(led_Dad, LOW);
+//        delay(time);
+//        //tone(pin_B,392,100);//392 숫자는 4옥타브 솔 음 
+//        digitalWrite(led_Mom, LOW);
+//        digitalWrite(led_Windy, LOW);
+//        digitalWrite(led_Dad, HIGH);
+//        delay(time);
 }
---
+++ encoder-test/Melody/Melody.ino
@@ -0,0 +1,76 @@
+/* Encoder Library - Basic Example
+ * http://www.pjrc.com/teensy/td_libs_Encoder.html
+ *
+ * This example code is in the public domain.
+ */
+
+#include <Encoder.h>
+#include "pitches.h"
+
+
+int melody[] = {
+
+          //1 //12  //캉캉
+          NOTE_C4,NOTE_C4, 
+          NOTE_D4,NOTE_F4,NOTE_E4,NOTE_D4, 
+          NOTE_G4,NOTE_D4, 
+          NOTE_G4,NOTE_A4,NOTE_E4,NOTE_F4,
+          //2 //14
+          NOTE_D4,NOTE_D4, 
+          NOTE_D4,NOTE_F4,NOTE_E4,NOTE_D4, 
+          NOTE_C4,NOTE_C5,NOTE_B4,NOTE_A4, 
+          NOTE_G4,NOTE_F4,NOTE_E4,NOTE_D4,
+          //3 //12
+          NOTE_C4,NOTE_C4, 
+          NOTE_D4,NOTE_F4,NOTE_E4,NOTE_D4, 
+          NOTE_G4,NOTE_G4, 
+          NOTE_G4,NOTE_A4,NOTE_E4,NOTE_F4,
+          //4 //12
+          NOTE_D4,NOTE_D4, 
+          NOTE_D4,NOTE_F4,NOTE_E4,NOTE_D4, 
+          NOTE_C4,NOTE_G4,NOTE_D4,NOTE_E4, 
+          NOTE_C4,NOTE_C5,
+
+//        //1 //16 //환희의 송가
+//        NOTE_E4,NOTE_E4,NOTE_F4,NOTE_G4, NOTE_G4,NOTE_F4,NOTE_E4,NOTE_D4, NOTE_C4,NOTE_C4,NOTE_D4,NOTE_E4, NOTE_E4,NOTE_D4,NOTE_D4,0,
+//        //2 //16
+//        NOTE_E4,NOTE_E4,NOTE_F4,NOTE_G4, NOTE_G4,NOTE_F4,NOTE_E4,NOTE_D4, NOTE_C4,NOTE_C4,NOTE_D4,NOTE_E4, NOTE_D4,NOTE_C4,NOTE_C4,0,
+//        //3 //18
+//        NOTE_D4,NOTE_D4,NOTE_E4,NOTE_C4, NOTE_D4,NOTE_E4,NOTE_F4,NOTE_E4,NOTE_C4, NOTE_D4,NOTE_E4,NOTE_F4,NOTE_E4,NOTE_D4, NOTE_C4,NOTE_D4,NOTE_D4,0,
+//        //4 //16
+//        NOTE_E4,NOTE_E4,NOTE_F4,NOTE_G4, NOTE_G4,NOTE_F4,NOTE_E4,NOTE_D4, NOTE_C4,NOTE_C4,NOTE_D4,NOTE_E4, NOTE_E4,NOTE_D4,NOTE_D4,0,
+
+//
+//        //1 //작은별
+//        NOTE_C4,NOTE_C4,NOTE_G4,NOTE_G4,NOTE_A4,NOTE_A4,NOTE_G4,NOTE_F4,NOTE_F4,NOTE_E4,NOTE_E4,NOTE_D4,NOTE_D4,NOTE_C4,
+//        //2
+//        NOTE_G4,NOTE_G4,NOTE_F4,NOTE_F4,NOTE_E4,NOTE_E4,NOTE_D4,NOTE_G4,NOTE_G4,NOTE_F4,NOTE_F4,NOTE_E4,NOTE_E4,NOTE_D4,
+//        //3
+//        NOTE_C4,NOTE_C4,NOTE_G4,NOTE_G4,NOTE_A4,NOTE_A4,NOTE_G4,NOTE_F4,NOTE_F4,NOTE_E4,NOTE_E4,NOTE_D4,NOTE_D4,NOTE_C4,0,
+//        
+};
+
+
+// Change these two numbers to the pins connected to your encoder.
+//   Best Performance: both pins have interrupt capability
+//   Good Performance: only the first pin has interrupt capability
+//   Low Performance:  neither pin has interrupt capability
+Encoder myEnc(5, 4);
+//   avoid using pins with LEDs attached
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Basic Encoder Test:");
+}
+
+long oldPosition  = -999;
+
+void loop() {
+  long newPosition = myEnc.read()/2;
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
+    tone(8,melody[newPosition%(12+14+12+12)],100);
+  }
+}
