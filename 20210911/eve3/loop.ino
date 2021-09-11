062cb13f48146fc38a4bbf43f34d371b5c12156e
richard <yaros0914@gmail.com>
2021-09-11 11:51:50 +0900

    update_2021-09-11 11:51:50

--- 20210911/eve3/loop.ino
+++ 20210911/eve3/loop.ino
@@ -2,19 +2,30 @@ void loop() {
   touchState = digitalRead(hit);
   Serial.println(touchState);
 
-  if (touchState == 1) {  // 안 닿아있으면
-    printNumber(0);
-  }
-  else {                  // 닿아있으면
-    printNumber(1);
-  }
+//  if (touchState == 1) {  // 안 닿아있으면
+//    printNumber(0);
+//  }
+//  else {                  // 닿아있으면
+//    printNumber(1);
+//  }
 
-  //countHit(); // 닿은 횟수 세기 (0~9, 10번 닿으면 펑!)
+//  countHit(); // 닿은 횟수 세기 (0~9, 10번 닿으면 펑!)
 
-  //printNumber(5); // 원하는 숫자 출력하기
+//  printNumber(3); // 원하는 숫자 출력하기
+//
+for (int j = 0; j < 9; j++) {
+  printShape(j);
+  delay(1000);
+}
 
-  //printShape(0); // 원하는 모양 출력하기
-
+//  printShape(6); // 원하는 모양 출력하기
+//  delay(1000);
+//  printShape(7);
+//  delay(1000);
+//  printShape(8);
+//  delay(1000);
+//  printShape(9);
+//  delay(1000);
 }
 
 
@@ -41,7 +52,7 @@ void printAll(int on1off0) {
 }
 
 void countHit() {
-  printNumber(count);
+//  printShape(count);
   if (touchState == 0) {
     if (flag == 0) {
       if (count < 11) {
@@ -54,31 +65,34 @@ void countHit() {
     flag = 0;
   }
   switch (count) {
-    case 1 : printNumber(1);
+     case 0 : printShape(0);
       delay(100);
       break;
-    case 2 : printNumber(2);
+    case 1 : printShape(1);
       delay(100);
       break;
-    case 3 : printNumber(3);
+    case 2 : printShape(2);
       delay(100);
       break;
-    case 4 : printNumber(4);
+    case 3 : printShape(3);
       delay(100);
       break;
-    case 5 : printNumber(5);
+    case 4 : printShape(4);
       delay(100);
       break;
-    case 6 : printNumber(6);
+    case 5 : printShape(5);
       delay(100);
       break;
-    case 7 : printNumber(7);
+    case 6 : printShape(6);
       delay(100);
       break;
-    case 8 : printNumber(8);
+    case 7 : printShape(7);
       delay(100);
       break;
-    case 9 : printNumber(9);
+    case 8 : printShape(8);
+      delay(100);
+      break;
+    case 9 : printShape(9);
       delay(100);
       break;
     case 10 : printAll(0);
